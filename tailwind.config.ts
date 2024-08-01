d2077c8b67d80e8c5cb4ad8d8155caf8f9890fb2
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2024-08-01 17:19:19 +0530

    chore: Initialize Next.js project structure and configuration files

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: {
+          primary: "rgb(var(--color-background-primary) , <alpha-value>)",
+          secondary: "rgb(var(--color-background-secondary), <alpha-value>)",
+        },
+        text: {
+          primary: "rgb(var(--color-text-primary), <alpha-value>)",
+          secondary: "rgb(var(--color-text-secondary, <alpha-value>))",
+          tertiary: "rgb(var(--color-text-tertiary), <alpha-value>)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
